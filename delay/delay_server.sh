b2143de0aa27c16e48e83c94b2cbbd352137b316
Simeon Tran <siatran@ucsc.edu>
2024-07-21 16:05:28 -0700

    Currently working on trying to edit sdkconfig

--- delay/delay_server.sh
+++ delay/delay_server.sh
@@ -1,4 +1,23 @@
-delay_server_path="$HOME/Desktop/Repositories/network-performance-ftd"
+# Command line format:
+#   sdkconfig_set [sdkconfig variable] [value] [sdkconfig path]
+#
+# Example:
+#   sdkconfig_set CONFIG_THREAD_ASCON_CIPHER_SUITE 0 ./sdkconfig
+#
+function sdkconfig_set() {
+  to_replace=$(cat $3 | grep $1)
+  sed -i "" "s/$to_replace/$1=$2/g" $3
+}
+
+# Command line format:
+#   sdkconfig_get [sdkconfig variable] [sdkconfig path]
+#
+# Example:
+#   sdkconfig_get CONFIG_THREAD_ASCON_CIPHER_SUITE ./sdkconfig
+#
+function sdkconfig_get() {
+  cat $2 | grep $1
+}
 
 # Get the TX Power and Encryption Algorithm to use from the command line arguments.
 #
@@ -14,15 +33,12 @@ do
   esac
 done
 
-# Command line format:
-#   sdkconfig_set [sdkconfig variable] [value] [sdkconfig path]
-#
-# Example:
-#   sdkconfig_set CONFIG_THREAD_ASCON_CIPHER_SUITE 0 ./sdkconfig
-#
-function sdkconfig_set() {
-  to_replace=$(cat sdkconfig | grep $1)
-  sed -i "" "s/$to_replace/$1=$2/g" $3
-}
+delay_server_path="$HOME/Desktop/Repositories/network-performance-ftd"
+
+sdkconfig_set CONFIG_THREAD_ASCON_CIPHER_SUITE CIPHER_NUM $delay_server_path/sdkconfig
+sdkconfig_set CONFIG_TX_POWER TX_POWER $delay_server_path/sdkconfig
+
+echo $(sdkconfig_get CONFIG_THREAD_ASCON_CIPHER_SUITE $delay_server_path/sdkconfig)
+echo $(sdkconfig_get CONFIG_TX_POWER $delay_server_path/sdkconfig)
 
 . $HOME/esp/esp-idf/export.sh > /dev/null

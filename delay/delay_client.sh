7065eb2ea18ead4e0d008e94646628066c26d716
Simeon Tran <siatran@ucsc.edu>
2024-07-21 17:37:26 -0700

    Created delay client

---
+++ delay/delay_client.sh
@@ -0,0 +1,66 @@
+# Command line format:
+#   sdkconfig_set [sdkconfig variable] [value] [sdkconfig path]
+#
+# Example:
+#   sdkconfig_set CONFIG_THREAD_ASCON_CIPHER_SUITE 0 ./sdkconfig
+#
+function sdkconfig_set() {
+  to_replace=$(cat $3 | grep $1=)
+  sed -i "" "s/$to_replace/$1=$2/g" $3
+}
+
+# Command line format:
+#   sdkconfig_get [sdkconfig variable] [sdkconfig path]
+#
+# Example:
+#   sdkconfig_get CONFIG_THREAD_ASCON_CIPHER_SUITE ./sdkconfig
+#
+function sdkconfig_get() {
+  cat $2 | grep $1=
+}
+
+# Get the TX Power and Encryption Algorithm to use from the command line arguments.
+#
+# The Bash script code that uses `optarg` to get the command line arguments
+# comes from:
+# https://www.baeldung.com/linux/use-command-line-arguments-in-bash-script#flags
+#
+while getopts t:e:p: arg
+do
+  case "${arg}" in
+    t) tx_power=${OPTARG};;
+    e) cipher_num=${OPTARG};;
+    p) delay_client_port=${OPTARG};;
+  esac
+done
+
+delay_client_path="$HOME/Desktop/Repositories/network-performance-ftd"
+delay_client_sdkconfig=$delay_client_path/sdkconfig
+
+# The value of `CONFIG_EXPERIMENT=4` will set the device to run as
+# the Delay server.
+#
+delay_client_flag=4
+sdkconfig_set CONFIG_EXPERIMENT $delay_client_flag $delay_client_sdkconfig
+
+# Make sure Time Synchronization is on.
+#
+time_sync_off="# CONFIG_OPENTHREAD_TIME_SYNC is not set"
+time_sync_on="CONFIG_OPENTHREAD_TIME_SYNC=y"
+sed -i "" "s/$time_sync_off/$time_sync_on/g" $delay_client_sdkconfig
+
+# Change both the cipher suite and TX power settings in `sdkconfig`.
+#
+sdkconfig_set CONFIG_THREAD_ASCON_CIPHER_SUITE $cipher_num $delay_client_sdkconfig
+sdkconfig_set CONFIG_TX_POWER $tx_power $delay_client_sdkconfig
+
+echo $(sdkconfig_get CONFIG_THREAD_ASCON_CIPHER_SUITE $delay_client_sdkconfig)
+echo $(sdkconfig_get CONFIG_TX_POWER $delay_client_sdkconfig)
+echo $(sdkconfig_get CONFIG_EXPERIMENT $delay_client_sdkconfig)
+echo $(sdkconfig_get CONFIG_OPENTHREAD_TIME_SYNC $delay_client_sdkconfig)
+
+. $HOME/esp/esp-idf/export.sh > /dev/null
+
+cd $delay_client_path
+idf.py build flash monitor --port $delay_client_port
+cd -

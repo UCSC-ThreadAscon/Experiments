1bac026336ed09790da46a523742cc8a1af43902
Simeon Tran <siatran@ucsc.edu>
2024-08-09 17:33:18 -0700

    Parsed the file for the averages.

--- delay/queue/parse.py
+++ delay/queue/parse.py
@@ -1,8 +1,12 @@
 import sys
 import os
 
+AVERAGE_DELAY_EXP="The AVERAGE delay is:"
+SPACE_DELIMITER=" "
+
 testFile = os.path.join(os.curdir, "delay-client-AES-20dBm.txt")
 
 with open(testFile, 'r') as file:
   for line in file:
-    print(line)
+    if AVERAGE_DELAY_EXP in line:
+      print(line.split(SPACE_DELIMITER)[7])

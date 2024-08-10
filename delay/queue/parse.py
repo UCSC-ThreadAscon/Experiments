418865a0ec7f0f24c8931c0eae195bcfe5c661d5
Simeon Tran <siatran@ucsc.edu>
2024-08-09 17:50:40 -0700

    Parsed all averages and put them in array.

--- delay/queue/parse.py
+++ delay/queue/parse.py
@@ -1,21 +1,24 @@
 import sys
 import os
 
-testFile = os.path.join(os.curdir, "delay-client-AES-20dBm.txt")
+def getAverage(filepath):
+  averages = []
 
-averages = []
+  with open(testFile, 'r') as file:
+    for line in file:
+      """ The average Delay (in uS) for each experiment will be
+          always displayed after the phrase 'The AVERAGE delay is'.
 
-with open(testFile, 'r') as file:
-  for line in file:
-    """ The average Delay (in uS) for each experiment will be
-        always displayed after the phrase 'The AVERAGE delay is'.
+          The average Delay will always be the 7th word in the line
+          that it is displayed in.
+      """
+      if "The AVERAGE delay is:" in line:
+        words = line.split(" ")
+        average = int(words[7])
+        averages.append(average)
 
-        The average Delay will always be the 7th word in the line
-        that it is displayed in.
-    """
-    if "The AVERAGE delay is:" in line:
-      words = line.split(" ")
-      average = int(words[7])
-      averages.append(average)
+  return averages
 
-print(averages)
+if __name__ == "__main__":
+  testFile = os.path.join(os.curdir, "delay-client-AES-20dBm.txt")
+  print(getAverage(testFile))

dd0bc3801fafa45f05aa23005c3aae463fed540f
Simeon Tran <siatran@ucsc.edu>
2024-08-09 20:09:40 -0700

    Still need to work on combining files.

--- delay/queue/combine.sh
+++ delay/queue/combine.sh
@@ -1,6 +1,10 @@
 logs=($(ls | grep "delay-client"))
 
-touch combined.txt
+# Grab the independent variables from the first file.
+cipher=$(cat ${logs[1]} | grep "Cipher Suite:" | awk '{print $6}' | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g' | tr -d '\000')
+tx_power=$(cat ${logs[1]} | grep "Max TX Power is:" -m 1 | awk '{print $8}')
+
+touch "delay-client-${cipher}-${tx_power}dbm-FULL-LOG.txt"
 
 for log in ${logs[@]}; do
   cat $log | tee -a combined.txt > /dev/null

8a41aae2f1d26b46ab7332584f3dc8503223fd05
Simeon Tran <siatran@ucsc.edu>
2024-08-09 18:41:40 -0700

    Retrieved the cipher and TX power

--- delay/queue/parse.py
+++ delay/queue/parse.py
@@ -5,14 +5,14 @@ import os
        write a Bash script that combines the multiple files into
        a single, large file.
 
-       While you're at it, use the Bash script to check the independent
-       variables, and to make sure the indepedent variables are consistent
-       across files.
+       While you're at it, use the Bash script to print the independent
+       variables.
 
     2. When calculating the average delay, only consider the first
        100 average delays in the array.
 
     3. Write the average you calculated into a new file.
+       * Parse the file with the average delays itself to get the independent variables.
 
     Use consistent and systematic naming conventions so you know which files
     to look for without having to specify them as input.
@@ -20,13 +20,13 @@ import os
 
 def getAverageDelays(filepath):
   averages = []
-  with open(testFile, 'r') as file:
+  with open(filepath, 'r') as file:
     for line in file:
       """ The average Delay (in uS) for each experiment will be
           always displayed after the phrase 'The AVERAGE delay is'.
 
-          The average Delay will always be the 7th word in the line
-          that it is displayed in.
+          The average Delay will always be the 7th word (assuming 0 index)
+          in the line that it is displayed in.
       """
       if "The AVERAGE delay is:" in line:
         words = line.split(" ")
@@ -40,10 +40,32 @@ def getFinalAverage(averages):
     listSum += average
   return listSum / len(averages)
 
+def findFirstLine(expression, filepath):
+  with open(filepath, 'r') as file:
+    for line in file:
+      if expression in line:
+        return line
+  return None
+
+def writeFinalAverage(averageDelays, finalAverge, filepath):
+  line = findFirstLine("Cipher Suite:", filepath)
+  words = line.split(" ")
+  print(words)
+  cipher = words[5]
+
+  line = findFirstLine("Max TX Power is:", filepath)
+  words = line.split(" ")
+  txPower = words[7]
+
+  print(cipher)
+  print(txPower)
+  return
+
 if __name__ == "__main__":
   testFile = os.path.join(os.curdir, "delay-client-AES-20dBm.txt")
   averages = getAverageDelays(testFile)
   finalAverage = getFinalAverage(averages)
 
-  print(averages)
-  print(finalAverage)
+  # print(averages)
+  # print(finalAverage)
+  writeFinalAverage(averages, finalAverage, testFile)

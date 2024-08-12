3fab400a0d6c836e23d32a6425226fc386c832b1
Simeon Tran <siatran@ucsc.edu>
2024-08-12 14:32:25 -0700

    Check delay server and net perf ftd

--- delay/check-versions.sh
+++ delay/check-versions.sh
@@ -1,7 +1,10 @@
 # The commit IDs that will be used in all Delay experiments.
 ESP_IDF_COMMIT=6377abb842705a5aa8feb00ef1996f236f64b5dd
 OPENTHREAD_COMMIT=a011df52f209726bf9b9dc5ae123ec53bceafe7e
-NET_PERF_FTD_COMMIT=f2d86f5210284c8cd54e3315bafb197513c18ab9
+
+# This commit ID is shared by both the Network Performance FTD and the Delay Server,
+# as they are both local copies of the same repository.
+DRIVER_CODE_FTD_COMMIT=f2d86f5210284c8cd54e3315bafb197513c18ab9
 
 # The location of each of the repositories.
 ESP_IDF_LOC=${HOME}/esp/esp-idf
@@ -37,7 +40,6 @@ function show_last_commit() {
 #
 function commit_id_check() {
   start_delimiter
-  printf "\nGoing to %s Local Repository.\n" $1
 
   cd $2
   git restore .
@@ -45,12 +47,12 @@ function commit_id_check() {
 
   if [ $local_commit = $3 ]
   then
-    printf "\n%s is using Commit ID: %s.\n" $1 $local_commit
+    printf "\n%s is using Commit ID: %s.\n\n" "$1" $local_commit
     show_last_commit
     end_delimiter
   else
-    printf "\n$1 Commit ID to use in Experiments: %s.\n" $3
-    printf "\n$1 Local Commit ID: %s.\n" $local_commit
+    printf "\n%s Commit ID to use in Experiments: %s.\n" "$1" $3
+    printf "\n%s Local Commit ID: %s.\n" "$1" $local_commit
     printf "\nThere is a Commit ID mismatch.\n"
     end_delimiter
     exit 1
@@ -59,3 +61,6 @@ function commit_id_check() {
 
 commit_id_check "ESP-IDF" $ESP_IDF_LOC $ESP_IDF_COMMIT
 commit_id_check "OpenThread" $OPENTHREAD_LOC $OPENTHREAD_COMMIT
+
+commit_id_check "Network Performance FTD" $NET_PERF_FTD_LOC $DRIVER_CODE_FTD_COMMIT
+commit_id_check "Delay Server" $DELAY_SERVER_LOC $DRIVER_CODE_FTD_COMMIT

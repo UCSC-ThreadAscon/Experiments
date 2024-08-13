d87892f271f30d97f0534199cf60d185df00abbf
Simeon Tran <siatran@ucsc.edu>
2024-08-12 18:51:23 -0700

    Do for all scripts

--- delay/check-versions.sh
+++ delay/check-versions.sh
@@ -61,7 +61,7 @@ function change_repo_commit() {
 }
 
 change_repo_commit "ESP-IDF" $ESP_IDF_LOC $ESP_IDF_COMMIT
-# change_repo_commit "OpenThread" $OPENTHREAD_LOC $OPENTHREAD_COMMIT
+change_repo_commit "OpenThread" $OPENTHREAD_LOC $OPENTHREAD_COMMIT
 
-# change_repo_commit "Network Performance FTD" $NET_PERF_FTD_LOC $DRIVER_CODE_FTD_COMMIT
-# change_repo_commit "Delay Server" $DELAY_SERVER_LOC $DRIVER_CODE_FTD_COMMIT
+change_repo_commit "Network Performance FTD" $NET_PERF_FTD_LOC $DRIVER_CODE_FTD_COMMIT
+change_repo_commit "Delay Server" $DELAY_SERVER_LOC $DRIVER_CODE_FTD_COMMIT

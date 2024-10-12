b42b2c1712943901135985bc15a6ccf6843d0907
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-12 14:44:39 -0700

    Working on building the RCP.

---
+++ throughput-confirmable/border_router.sh
@@ -0,0 +1,62 @@
+# Command Line Format:
+#
+#   ./border_router.sh [USB Modem port number of border router]
+#
+# Command line format:
+#   sdkconfig_set [sdkconfig variable] [value] [sdkconfig path]
+#
+# Example:
+#   sdkconfig_set CONFIG_THREAD_ASCON_CIPHER_SUITE 0 ./sdkconfig
+#
+# https://unix.stackexchange.com/questions/159367/using-sed-to-find-and-replace'
+# https://stackoverflow.com/a/57766728/6621292
+#
+function sdkconfig_set() {
+  to_replace=$(cat $3 | grep $1=)
+  sed -i -e "s/$to_replace/$1=$2/g" $3
+}
+
+# Command line format:
+#   sdkconfig_get [sdkconfig variable] [sdkconfig path]
+#
+# Example:
+#   sdkconfig_get CONFIG_THREAD_ASCON_CIPHER_SUITE ./sdkconfig
+#
+function sdkconfig_get() {
+  cat $2 | grep $1=
+}
+
+function to_cipher_string() {
+  case $1 in
+    0) echo "AES" ;;
+    1) echo "NoEncrypt" ;;
+    2) echo "Ascon128a-esp32" ;;
+    3) echo "Ascon128a-ref" ;;
+    4) echo "LibAscon-128a" ;;
+    5) echo "LibAscon-128" ;;
+  esac
+}
+
+# https://www.baeldung.com/linux/use-command-line-arguments-in-bash-script#flags
+while getopts t:e:p: arg
+do
+  case "${arg}" in
+    t) tx_power=${OPTARG};;
+    e) cipher_num=${OPTARG};;
+    p) border_router_port=${OPTARG};;
+  esac
+done
+
+. $HOME/esp/esp-idf/export.sh
+
+# ---- Build the RCP ----
+rcp_path="$IDF_PATH/examples/openthread/ot_rcp"
+rcp_sdkconfig=$rcp_path/sdkconfig
+
+sdkconfig_set CONFIG_THREAD_ASCON_CIPHER_SUITE $cipher_num $rcp_sdkconfig
+echo $(sdkconfig_get CONFIG_THREAD_ASCON_CIPHER_SUITE $rcp_sdkconfig)
+
+cd $rcp_path
+idf.py fullclean
+idf.py build
+# -----------------------

9710404222a8337d7041d3e4ed589a1662a85a0e
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-12 13:41:22 -0700

    Set commit IDs script for throughput confirmable experiment.

---
+++ throughput-confirmable/set_commit_ids.sh
@@ -0,0 +1,121 @@
+# The commit ID that will be used in all Delay experiments.
+ESP_IDF_COMMIT=aa8f5f533667ec3dcf446d2cc5c8e785cc09676b
+
+# The commit ID of the FTD.
+FTD_COMMIT=c0631ec03866221e07761b486b086e901eed11ec
+
+# The commit ID of the border router
+BORDER_ROUTER_COMMIT=9f99cacc2f7fca22190276847d55235818e85b2b
+
+# The location of each of the repositories.
+ESP_IDF_LOC=${HOME}/esp/esp-idf
+OPENTHREAD_LOC=${HOME}/esp/esp-idf/components/openthread/openthread
+FTD_LOC=$HOME/Desktop/Repositories/network-performance-ftd
+BORDER_ROUTER_LOC=$HOME/Desktop/Repositories/br_netperf/examples/basic_thread_border_router
+
+function print_delimiter() {
+  echo "-----------------------------------------------------------------------------------------"
+}
+
+function show_last_commit() {
+  git --no-pager log -n1
+}
+
+# Command Format:
+#   change_repo_commit [name of repo as string] [path to local repo] [expected experiment commit id]
+#
+#   [name of repo as string] =          $1
+#   [path to local repo] =              $2
+#   [expected experiment commit id] =   $3
+#
+# Example:
+#   change_repo_commit "ESP-IDF" "$ESP_IDF_LOC" "$ESP_IDF_COMMIT"
+#
+# Sources Used:
+#   https://stackoverflow.com/a/45652159/6621292
+#   https://stackoverflow.com/a/43854593/6621292
+#   https://stackoverflow.com/a/7737071/6621292
+#   https://stackoverflow.com/a/61751340/6621292
+#
+function change_repo_commit() {
+  print_delimiter
+
+  # Go to the local Git repository.
+  cd $2
+  printf "Currently in repository: %s\n\n" "$(pwd)"
+
+  # Clean the repo of all unstaged changes.
+  git restore . --recurse-submodules
+  echo "Did a GIT RESTORE (with --recurse-submodules) to clear all unstaged changes."
+
+  printf "\nCommit BEFORE Checkout: %s.\n" "$(git rev-parse HEAD)"
+
+  # Change repo to the version that we want to use in the experiments.
+  git -c advice.detachedHead=false checkout --recurse-submodules $3
+  echo "Completed git checkout (with --recurse-submodules)."
+
+  # Prove that the repository is at the correct commit ID,
+  # and that no unstaged changes have been made to the repository.
+  #
+  printf "Repository is now at EXPERIMENT COMMIT ID: %s.\n\n" "$(git rev-parse HEAD)"
+
+  git status
+  printf "\n"
+
+  git --no-pager log --pretty=oneline -n1
+  print_delimiter
+}
+
+# Command Format:
+#   print_commit [name of repo as string] [path to local repo]
+#
+# Sources Used:
+#   https://stackoverflow.com/a/7737071/6621292
+#   https://stackoverflow.com/a/7737071/6621292
+#
+function print_commit() {
+  print_delimiter
+
+  cd $2
+  printf "Currently in repository '%s': %s\n\n" "$1" "$(pwd)"
+  printf "Repository is currently at COMMIT ID: %s.\n\n" "$(git rev-parse HEAD)"
+
+  git --no-pager log --pretty=oneline -n1
+
+  print_delimiter
+}
+
+# https://www.atatus.com/blog/bash-scripting/
+setup_ftd=false
+setup_border_router=false
+while getopts "cs" flag; do
+  case $flag in
+    c)
+      echo "Setting the Commit IDs for the FTD."
+      setup_ftd=true
+      ;;
+    s)
+      echo "Setting the Commit IDs for the BORDER ROUTER."
+      setup_border_router=true
+      ;;
+    \?)
+      "Flag -${flag} is in invalid option."
+      exit 1
+  esac
+done
+
+# Move ESP-IDF and its submodules to correct commit ID, then
+# show commit ID of OpenThread submodule.
+#
+change_repo_commit "ESP-IDF" $ESP_IDF_LOC $ESP_IDF_COMMIT
+print_commit "OpenThread" $OPENTHREAD_LOC
+
+if $setup_ftd;
+then
+  change_repo_commit "Network Performance FTD" $FTD_COMMIT $FTD_LOC
+fi
+
+if $setup_border_router;
+then
+  change_repo_commit "Border Router" $BORDER_ROUTER_LOC $BORDER_ROUTER_COMMIT
+fi

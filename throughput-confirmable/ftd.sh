22004f2dbb8a2249376c9a43499f4183df3f6539
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-14 15:24:59 -0700

    Distinguish between border router and FTD files.

---
+++ throughput-confirmable/ftd.sh
@@ -0,0 +1,50 @@
+# https://unix.stackexchange.com/questions/159367/using-sed-to-find-and-replace
+# https://stackoverflow.com/a/57766728/6621292
+#
+function sdkconfig_set() {
+  to_replace=$(cat $3 | grep $1=)
+  sed -i -e "s/$to_replace/$1=$2/g" $3
+}
+
+# Command line format:
+#   sdkconfig_get [sdkconfig variable] [sdkconfig path]
+#
+# Example:
+#   sdkconfig_get CONFIG_THREAD_ASCON_CIPHER_SUITE ./sdkconfig
+#
+function sdkconfig_get() {
+  cat $2 | grep $1=
+}
+
+function to_cipher_string() {
+  case $1 in
+    0) echo "AES" ;;
+    1) echo "NoEncrypt" ;;
+    2) echo "Ascon128a-esp32" ;;
+    3) echo "Ascon128a-ref" ;;
+    4) echo "LibAscon-128a" ;;
+    5) echo "LibAscon-128" ;;
+  esac
+}
+
+# https://www.baeldung.com/linux/use-command-line-arguments-in-bash-script#flags
+while getopts t:e:p: arg
+do
+  case "${arg}" in
+    t) tx_power=${OPTARG};;
+    e) cipher_num=${OPTARG};;
+    p) ftd_port=${OPTARG};;
+  esac
+done
+
+# ---- Create the Output File ----
+cipher_string=$(to_cipher_string $cipher_num)
+txpower_string="${tx_power}dbm"
+
+output_file_path="$HOME/Desktop/Repositories/Experiments/throughput-confirmable/queue/tp-con-FTD-$cipher_string-$txpower_string.txt"
+rm -f $output_file_path
+date | tee $output_file_path
+
+set_commit_ids_exec=$HOME/Desktop/Repositories/Experiments/throughput-confirmable/set_commit_ids.sh
+$set_commit_ids_exec -s | tee -a $output_file_path
+# --------------------------------

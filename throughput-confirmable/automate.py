2858971ed5ceacee462e75f3df81d12f737ea951
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-31 13:03:02 -0700

    Subprocess run() works

--- throughput-confirmable/automate.py
+++ throughput-confirmable/automate.py
@@ -1,4 +1,6 @@
-from subprocess import Popen
+import subprocess
 
-print(Popen(["make", "clean-queue"]))
-print(Popen(["make", "tp-con-border-router-aes-20"]))
+subprocess.run(["make", "clean-queue"])
+
+subprocess.run(["make", "tp-con-border-router-aes-20"])
+# subprocess.Popen(["make", "tp-con-ftd-aes-20"])

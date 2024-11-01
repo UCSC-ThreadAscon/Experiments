2096fdb566b7b585258d423988f845db7b847560
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-11-01 13:03:43 -0700

    Using PIPE doesn't work, as ESP-IDF monitor needs a terminal stream for it to work.

--- throughput-confirmable/main.py
+++ throughput-confirmable/main.py
@@ -1,7 +1,12 @@
-from subprocess import Popen, run, STDOUT
+"""RESOURCES UTILIZED
+   https://shuzhanfan.github.io/2017/12/parallel-processing-python-subprocess/
+"""
+
+from subprocess import Popen, run, STDOUT, PIPE
 
 if __name__ == "__main__":
   run(["make", "clean-queue"])
 
-  process = Popen(["make", "tp-con-border-router-aes-20"], stderr=STDOUT)
-  process.wait()
+  br_process = Popen(["make", "tp-con-border-router-aes-20"], stderr=STDOUT)
+
+  # br_process.wait()

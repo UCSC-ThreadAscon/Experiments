a80731d0121be752f701b53cf820e5e76aa6cfc4
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-11-01 12:37:00 -0700

    Got the script working

--- throughput-confirmable/main.py
+++ throughput-confirmable/main.py
@@ -1,8 +1,7 @@
 from subprocess import Popen, run, STDOUT
 
 if __name__ == "__main__":
-  # subprocess.run(["make", "clean-queue"])
+  run(["make", "clean-queue"])
 
-  process = Popen(["bash", "./border_router.sh", "-t" ,"20", "-e", "0", "-p", "/dev/cu.usbmodem2101"],
-                  stderr=STDOUT)
+  process = Popen(["make", "tp-con-border-router-aes-20"], stderr=STDOUT)
   process.wait()

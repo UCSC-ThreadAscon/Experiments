028497cbefdfb681dc67a07c1a4f8e43df0d9ef3
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-11-01 15:39:01 -0700

    Leaving the Beazley function as is.

--- throughput-confirmable/main.py
+++ throughput-confirmable/main.py
@@ -7,15 +7,14 @@
 from subprocess import Popen, run, STDOUT, PIPE
 from time import sleep
 
-SLEEP_TIME_SECONDS = 0.1
-
 SERVER_START_STRING = "Started CoAP server at port 5683."
 
 """ Slides 75-79 of https://www.dabeaz.com/generators/Generators.pdf.
 """
-def beazleyRealTimeFileRead(filename, seconds):
+def beazleyRealTimeFileRead(filename):
   OFFSET = 0
   FILE_END = 2
+  SLEEP_TIME_SECONDS = 0.1
 
   while True:
     try:
@@ -23,18 +22,16 @@ def beazleyRealTimeFileRead(filename, seconds):
         file.seek(OFFSET, FILE_END)
 
         for line in file:
-          print(line)
           yield line
-          offset += 1
         else:
-          sleep(seconds)
+          sleep(SLEEP_TIME_SECONDS)
     except OSError:
       #
       # This error occurs when the file does not exist yet.
       # Let's wait for a some time to allow for the subprocess
       # to create the file we want to read.
       #
-      sleep(seconds)
+      sleep(SLEEP_TIME_SECONDS)
       continue
   return
 
@@ -44,8 +41,7 @@ if __name__ == "__main__":
   br_process = Popen(["make", "tp-con-border-router-aes-20"], stderr=STDOUT)
   ftd_process = None
 
-  for line in beazleyRealTimeFileRead("./queue/tp-con-BR-AES-20dbm.txt",
-                                      SLEEP_TIME_SECONDS):
+  for line in beazleyRealTimeFileRead("./queue/tp-con-BR-AES-20dbm.txt"):
     if SERVER_START_STRING in line:
       ftd_process = Popen(["make", "tp-con-ftd-aes-20"], stderr=STDOUT)
       break

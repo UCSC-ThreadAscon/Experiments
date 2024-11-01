4d4ced776d587cfd78bed95bfd5b04b7c1109aef
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-11-01 14:29:40 -0700

    Got the print statement to trigger when the server starts.

--- throughput-confirmable/main.py
+++ throughput-confirmable/main.py
@@ -14,7 +14,7 @@ SERVER_START_STRING = "Started CoAP server at port 5683."
 
 """ Slides 75-79 of https://www.dabeaz.com/generators/Generators.pdf.
 """
-def beazleyRealTimeFileRead(filename):
+def beazleyRealTimeFileRead(filename, seconds):
   offset = FILE_START
 
   while True:
@@ -26,14 +26,14 @@ def beazleyRealTimeFileRead(filename):
           yield line
           offset += 1
         else:
-          sleep(SLEEP_TIME_SECONDS)
+          sleep(seconds)
 
     except OSError:
       # This error occurs when the file does not exist yet.
       # Let's wait for a some time to allow for the subprocess
       # to create the file we want to read.
       #
-      sleep(SLEEP_TIME_SECONDS)
+      sleep(seconds)
       continue
 
   return
@@ -44,7 +44,10 @@ if __name__ == "__main__":
   br_process = Popen(["make", "tp-con-border-router-aes-20"], stderr=STDOUT)
   ftd_process = None
 
-  for line in beazleyRealTimeFileRead("./queue/tp-con-BR-AES-20dbm.txt"):
-    pass
+  for line in beazleyRealTimeFileRead("./queue/tp-con-BR-AES-20dbm.txt",
+                                      SLEEP_TIME_SECONDS):
+    if SERVER_START_STRING in line:
+      print("--- TODO: Start the FTD Bash Script. ---")
+      break
 
   br_process.wait()

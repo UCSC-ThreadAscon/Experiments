20fbc1204b0021b2121ee0c0d1b1f47fc26ef7f7
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-11-01 13:55:34 -0700

    Going to have to do the Python method.

--- throughput-confirmable/main.py
+++ throughput-confirmable/main.py
@@ -1,12 +1,24 @@
 """RESOURCES UTILIZED:
+   https://www.dabeaz.com/generators/Generators.pdf
    https://shuzhanfan.github.io/2017/12/parallel-processing-python-subprocess/
 """
 
 from subprocess import Popen, run, STDOUT, PIPE
+from time import sleep
+
+
+""" Slides 77-79 of https://www.dabeaz.com/generators/Generators.pdf.
+"""
+def beazleyRealTimeFileRead(filename):
+  return
 
 if __name__ == "__main__":
   run(["make", "clean-queue"])
 
-  br_process = Popen(["make", "tp-con-border-router-aes-20"], stderr=STDOUT)
+  br_process = Popen(["bash", "./border_router.sh", "-t", "20", "-e", "0", "-p", "/dev/cu.usbmodem2101"], stderr=STDOUT)
 
-  # br_process.wait()
+  while True:
+    print("Hello")
+    sleep(5)
+
+  br_process.wait()

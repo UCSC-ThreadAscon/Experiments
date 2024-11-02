19452590e71cfe6321debd46abf372a81abc591f
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-11-02 13:48:30 -0700

    Pyserial works

--- throughput-confirmable/main.py
+++ throughput-confirmable/main.py
@@ -3,7 +3,7 @@
    https://shuzhanfan.github.io/2017/12/parallel-processing-python-subprocess/
    https://docs.python.org/3/tutorial/inputoutput.html#methods-of-file-objects
 """
-
+import serial
 from subprocess import Popen, run, STDOUT, PIPE
 from time import sleep
 
@@ -44,26 +44,37 @@ def beazleyRealTimeFileRead(filename, seconds=0.1):
 if __name__ == "__main__":
   run(["make", "clean-queue"])
 
-  br_process = Popen(["bash", "./border_router.sh", "-t", "20", "-e", "0", "-p", BORDER_ROUTER_PORT], stderr=STDOUT)
-  ftd_process = None
+  # br_process = Popen(["bash", "./border_router.sh", "-t", "20", "-e", "0", "-p", BORDER_ROUTER_PORT], stderr=STDOUT)
+  # ftd_process = None
 
-  for line in beazleyRealTimeFileRead("./queue/tp-con-BR-AES-20dbm.txt", 1):
-    if SERVER_START_STRING in line:
-      ftd_process = Popen(["bash", "./ftd.sh", "-t", "20", "-e", "0", "-p", FTD_PORT], stderr=STDOUT)
-      break
+  with serial.Serial(BORDER_ROUTER_PORT, timeout=1) as ser:
+    while True:
+      line = ser.readline()
+      print(line)
 
-  if ftd_process == None:
-    raise Exception("ERROR: Failed to start the FTD.")
+      # if SERVER_START_STRING in line:
+      #   print("---- STARTING FTD ----")
+      #   break
 
-  for line in beazleyRealTimeFileRead("./queue/tp-con-FTD-AES-20dbm.txt", 1):
-    #
-    # TO-DO: You need to terminate the ESP-IDF terminal by typing "CTRL" + "]"
-    #        onto the respective consoles, rather than sending a terminate signal.
-    #
-    if EXPERIMENT_END_STRING in line:
-      ftd_process.terminate()
-      br_process.terminate()
-      break
 
-  ftd_process.wait()
+  # for line in beazleyRealTimeFileRead("./queue/tp-con-BR-AES-20dbm.txt", 1):
+  #   if SERVER_START_STRING in line:
+  #     print("---- STARTING FTD ----")
+  #     ftd_process = Popen(["bash", "./ftd.sh", "-t", "20", "-e", "0", "-p", FTD_PORT], stderr=STDOUT)
+  #     break
+
+  # if ftd_process == None:
+  #   raise Exception("ERROR: Failed to start the FTD.")
+
+  # for line in beazleyRealTimeFileRead("./queue/tp-con-FTD-AES-20dbm.txt", 1):
+  #   #
+  #   # TO-DO: You need to terminate the ESP-IDF terminal by typing "CTRL" + "]"
+  #   #        onto the respective consoles, rather than sending a terminate signal.
+  #   #
+  #   if EXPERIMENT_END_STRING in line:
+  #     ftd_process.terminate()
+  #     br_process.terminate()
+  #     break
+
+  # ftd_process.wait()
   br_process.wait()

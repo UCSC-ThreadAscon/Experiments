6e887082622ea05496aa51a1a7d7b0a3c5266ee2
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-11-02 16:34:07 -0700

    Print statements to show status of what's going on.

--- throughput-confirmable/main.py
+++ throughput-confirmable/main.py
@@ -4,7 +4,7 @@
    https://docs.python.org/3/tutorial/inputoutput.html#methods-of-file-objects
 """
 import serial
-from subprocess import run, STDOUT
+from subprocess import run, STDOUT, PIPE
 from multiprocessing import Process
 from nrf802154_sniffer import Nrf802154Sniffer
 
@@ -12,6 +12,8 @@ from nrf802154_sniffer import Nrf802154Sniffer
 # 1. Automate Wireshark
 # 2. Add ability to specify different independent variables at cmd line args
 #
+
+SHOW_LOGS = False
 
 BORDER_ROUTER_PORT = "/dev/cu.usbmodem2101"
 FTD_PORT = "/dev/cu.usbmodem1201"
@@ -23,12 +25,14 @@ SERVER_START_STRING = "Created Throughput Confirmable server at 'throughput-conf
 EXPERIMENT_END_STRING = "Finished running 1 trials for current experiment."
 
 def ftd_monitor():
-  run(["bash", "./ftd.sh", "-t", "20", "-e", "0", "-p", FTD_PORT], stderr=STDOUT)
+  run(["bash", "./ftd.sh", "-t", "20", "-e", "0", "-p", FTD_PORT],
+      stdout=PIPE, stderr=STDOUT)
 
   log_filename = "queue/tp-con-FTD-AES-20dbm.txt"
 
   with open(log_filename, "ba") as logfile:
     with serial.Serial(FTD_PORT, timeout=1) as ftd:
+      print("FTD monitoring has started.")
       while True:
         line_bytes = ftd.readline()
 
@@ -36,23 +40,29 @@ def ftd_monitor():
           logfile.write(line_bytes)
 
           line = line_bytes.decode()
-          print(line.strip("\n"))
+          if SHOW_LOGS:
+            print(line.strip("\n"))
 
           if EXPERIMENT_END_STRING in line:
+            print("FTD has completed the experiment.")
             break
   return
 
 def border_router_monitor():
-  run(["bash", "./border_router.sh", "-t", "20", "-e", "0", "-p", BORDER_ROUTER_PORT], stderr=STDOUT)
+  run(["bash", "./border_router.sh", "-t", "20", "-e", "0", "-p", BORDER_ROUTER_PORT],
+      stdout=PIPE, stderr=STDOUT)
 
   log_filename = "queue/tp-con-BR-AES-20dbm.txt"
 
   sniffer = Nrf802154Sniffer()
   sniffer.extcap_capture(fifo="queue/tp-con-AES-20dbm.pcapng", dev=SNIFFER_PORT,
                          channel=THREAD_NETWORK_CHANNEL)
+  print("Started 802.15.4 Packet Sniffer Wireshark capture.")
 
   with open(log_filename, "ba") as logfile:
     with serial.Serial(BORDER_ROUTER_PORT, timeout=1) as border_router:
+      print("Border Router monitoring has started.")
+
       ftd_process = Process(target=ftd_monitor)
       ftd_started = False
 
@@ -63,14 +73,18 @@ def border_router_monitor():
           logfile.write(line_bytes)
 
           line = line_bytes.decode()
-          print(line.strip("\n"))
+          if SHOW_LOGS:
+            print(line.strip("\n"))
 
           if not ftd_started:
             if SERVER_START_STRING in line:
               ftd_process.start()
               ftd_started = True
-  
+
+  print("Border Router monitoring has stopped.")  
+
   sniffer.stop_sig_handler()
+  print("Stopped Packet Sniffer capture.")
   return
 
 if __name__ == "__main__":

b6a00f4f597e76c3f7af18d9eb8704efcabfb64b
Simeon <4740112+SimeonAT@users.noreply.github.com>
2024-11-09 20:16:00 -0800

    Writing function to check whether or not USB ports of main USB hub is on.

--- common/kasa_wrapper.py
+++ common/kasa_wrapper.py
@@ -28,3 +28,21 @@ async def power_off_all_devices():
   await power_off("Full Thread Device")
   await power_off("Packet Sniffer")
   return
+
+""" All USB ports in the Main USB Hub has on/off switches to toggle
+    on/off the power of a given USB port. Even though the power for a given
+     USB port is off, the Main USB Hub will still allow USB data transfer
+     for that USB port.
+
+    There are 4 smaller USB hubs that are connected to the USB Hub.
+    We do not want the ports Main USB Hub to powered on at the same time
+    as the smaller USB Hubs, as it may potentially supply too much power
+    to the MCUs (each of which are connected to the smaller USB hubs).
+
+    As a result, we want to check that for each of the 4 smaller USB hubs,
+    that the corresponding port that it is plugged into in the Main USB Hub
+    is off. If this is not the case, we end the experiment and power of all
+    of the smart plugs powering the USB hubs.
+"""
+async def check_main_usb_hub_ports_off():
+  return

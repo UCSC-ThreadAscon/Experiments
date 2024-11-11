516f3ba64fd08952e2f7b0b39d09f5d30228b3c9
Simeon <4740112+SimeonAT@users.noreply.github.com>
2024-11-11 15:58:32 -0800

    Brining back all the tests.

--- common/test.py
+++ common/test.py
@@ -7,18 +7,18 @@ def show_instructions(instructions):
   input()
   return
 
-# @pytest.mark.asyncio(loop_scope="module")
-# async def test_main_usb_hub_all_ports_off(capsys):
-#   with capsys.disabled():
-#     instructions = "Please POWER OFF ALL PORTS on the Main USB Hub."
-#     show_instructions(instructions)
+@pytest.mark.asyncio(loop_scope="module")
+async def test_main_usb_hub_all_ports_off(capsys):
+  with capsys.disabled():
+    instructions = "Please POWER OFF ALL PORTS on the Main USB Hub."
+    show_instructions(instructions)
 
-#   await check_main_usb_hub_ports_off()
+  await check_main_usb_hub_ports_off()
 
-#   stdout = capsys.readouterr().out
-#   assert("Main USB Hub has no ports powered on." in stdout)
-#   assert("All devices have been powered off. Ready to begin experiment." in stdout)
-#   return
+  stdout = capsys.readouterr().out
+  assert("Main USB Hub has no ports powered on." in stdout)
+  assert("All devices have been powered off. Ready to begin experiment." in stdout)
+  return
 
 @pytest.mark.asyncio(loop_scope="module")
 async def test_main_usb_hub_three_ports_on():

72e376d2bf2515af825a29465b77f0fd0d4bdb28
Simeon <4740112+SimeonAT@users.noreply.github.com>
2024-11-11 15:02:42 -0800

    Need to add loop_scope=module to run multiple async tests.

--- common/test.py
+++ common/test.py
@@ -1,21 +1,30 @@
 from kasa_wrapper import *
 import pytest
 
-def show_instructions(capsys, instructions):
+def show_instructions(instructions):
   instructions += " After doing so, press any key to continue."
-  with capsys.disabled():
-    print(instructions)
-    input()
+  print(instructions)
+  input()
   return
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(loop_scope="module")
 async def test_main_usb_hub_all_ports_off(capsys):
-  instructions = "Please power off ALL ports on the Main USB Hub."
-  show_instructions(capsys, instructions)
+  with capsys.disabled():
+    instructions = "Please POWER OFF ALL PORTS on the Main USB Hub."
+    show_instructions(instructions)
 
   await check_main_usb_hub_ports_off()
 
   stdout = capsys.readouterr().out
   assert("Main USB Hub has no ports powered on." in stdout)
   assert("All devices have been powered off. Ready to begin experiment." in stdout)
+  return
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_main_usb_hub_sniffer_on():
+  instructions = "Please POWER ON ONLY the NRF SNIFFER port on the Main USB Hub."
+  show_instructions(instructions)
+
+  with pytest.raises(AssertionError):
+    await check_main_usb_hub_ports_off()
   return

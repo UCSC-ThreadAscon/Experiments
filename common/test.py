eed28da2c922f808eb031f8f56b5d68c73eb4154
Simeon <4740112+SimeonAT@users.noreply.github.com>
2024-11-11 14:45:04 -0800

    Helper function for tests

--- common/test.py
+++ common/test.py
@@ -1,13 +1,18 @@
 from kasa_wrapper import *
 import pytest
 
-@pytest.mark.asyncio
-async def test_main_usb_hub_all_ports_off(capsys):
+def show_instructions(capsys):
   with capsys.disabled():
     instructions = "Please power off ALL ports on the Main USB Hub. " + \
                    "After doing so, press any key to continue."
     print(instructions)
     input()
+  return
+
+
+@pytest.mark.asyncio
+async def test_main_usb_hub_all_ports_off(capsys):
+  show_instructions(capsys)
 
   await check_main_usb_hub_ports_off()
 

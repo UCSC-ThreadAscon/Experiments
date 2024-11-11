94a248ea71eb7717103d3a90e441061817106da3
Simeon <4740112+SimeonAT@users.noreply.github.com>
2024-11-11 14:43:58 -0800

    Success test case.

--- common/test.py
+++ common/test.py
@@ -1,7 +1,17 @@
-def func(x):
-   return x + 1
+from kasa_wrapper import *
+import pytest
 
+@pytest.mark.asyncio
+async def test_main_usb_hub_all_ports_off(capsys):
+  with capsys.disabled():
+    instructions = "Please power off ALL ports on the Main USB Hub. " + \
+                   "After doing so, press any key to continue."
+    print(instructions)
+    input()
 
-def test_answer():
-   input("Press any key to continue.")
-   return True
+  await check_main_usb_hub_ports_off()
+
+  stdout = capsys.readouterr().out
+  assert("Main USB Hub has no ports powered on." in stdout)
+  assert("All devices have been powered off. Ready to begin experiment." in stdout)
+  return

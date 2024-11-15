ef7ac39235cd772137231792472ad702572279c2
Simeon <4740112+SimeonAT@users.noreply.github.com>
2024-11-15 11:20:18 -0800

    Get NAS info from env

--- common/synology.py
+++ common/synology.py
@@ -1,3 +1,10 @@
 from synology_api import filestation
-from dotenv import load_env
+from dotenv import load_dotenv
+from os import getenv
 
+load_dotenv()
+
+SYNOLOGY_IP = getenv("SYNOLOGY_IP")
+SYNOLOGY_PORT = getenv("SYNOLOGY_PORT")
+SYNOLOGY_USERNAME = getenv("SYNOLOGY_USERNAME")
+SYNOLOGY_PASSWORD = getenv("SYNOLOGY_PASSWORD")

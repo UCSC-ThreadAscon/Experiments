8207e3b3802b55a93b7f7f6719ea24c265deea16
Simeon <4740112+SimeonAT@users.noreply.github.com>
2024-11-15 11:45:12 -0800

    Need one time password

--- common/synology.py
+++ common/synology.py
@@ -1,6 +1,7 @@
 from synology_api import filestation
 from dotenv import load_dotenv
 from os import getenv
+import argparse
 
 load_dotenv()
 
@@ -8,3 +9,14 @@ SYNOLOGY_IP = getenv("SYNOLOGY_IP")
 SYNOLOGY_PORT = getenv("SYNOLOGY_PORT")
 SYNOLOGY_USERNAME = getenv("SYNOLOGY_USERNAME")
 SYNOLOGY_PASSWORD = getenv("SYNOLOGY_PASSWORD")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--otp",
+                    helper="The one time password given by the Synology Secure SignIn app.",
+                    required=True)
+
+fs = filestation.FileStation(SYNOLOGY_IP, SYNOLOGY_PORT, SYNOLOGY_USERNAME,
+                             SYNOLOGY_PASSWORD, secure=True, cert_verify=True,
+                             debug=True, dsm_version=7, otp_code=parser.otp)
+
+print(fs.get_info())

bb99a04e866279cc5aabc6b6a5fc0bdbfd424877
Simeon <4740112+SimeonAT@users.noreply.github.com>
2024-11-15 12:50:28 -0800

    Recursive function for uploading folder.

--- common/synology.py
+++ common/synology.py
@@ -1,24 +1,78 @@
 from synology_api import filestation
 from dotenv import load_dotenv
-from os import getenv
+from pathlib import Path
 import argparse
+import os
+
+from experiment import get_dir_path, Experiment
 
 load_dotenv()
 
-SYNOLOGY_IP = getenv("SYNOLOGY_IP")
-SYNOLOGY_PORT = getenv("SYNOLOGY_PORT")
-SYNOLOGY_USERNAME = getenv("SYNOLOGY_USERNAME")
-SYNOLOGY_PASSWORD = getenv("SYNOLOGY_PASSWORD")
+SYNOLOGY_IP = os.getenv("SYNOLOGY_IP")
+SYNOLOGY_PORT = os.getenv("SYNOLOGY_PORT")
+SYNOLOGY_USERNAME = os.getenv("SYNOLOGY_USERNAME")
+SYNOLOGY_PASSWORD = os.getenv("SYNOLOGY_PASSWORD")
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument("--otp",
                     help="The one time password given by the Synology Secure SignIn app.",
                     required=True)
 
+def get_nas_exp_dir_string(experiment_enum):
+  dirname = None
+  match experiment_enum:
+    case Experiment.DELAY.value:
+      dirname = "Delay-Experiments"
+    case Experiment.THROUGHPUT_CONFIRMABLE.value:
+      dirname = "Throughput-Confirmable-Experiments"
+    case _:
+      raise Exception(f"{experiment_enum} is not a valid Experiment Enum.")
+  return "/home/Master's Thesis/" + dirname + "/Results"
+
+""" RESCOURCES UTILIZED
+    https://www.python-engineer.com/posts/check-if-file-exists
+    https://stackoverflow.com/a/48191073
+"""
+def upload_folder(fs, local_path, nas_path_str):
+  for element in local_path.iterdir():
+
+    if element.is_dir():
+      fs.create_folder(folder_path=nas_path_str, name=element.name)
+    
+      local_path = local_path / element.name
+      nas_path_str += f"/{element.name}"
+      upload_folder(fs, local_path, nas_path_str)
+    
+    elif element.is_file():
+      fs.upload_file(dest_path=nas_path_str,
+                     file_path=local_path.as_posix(),
+                     overwrite=False,
+                     progress_bar=True)
+
+  return
+
 
 if __name__ == "__main__":
-  fs = filestation.FileStation(SYNOLOGY_IP, SYNOLOGY_PORT, SYNOLOGY_USERNAME,
-                              SYNOLOGY_PASSWORD, secure=True, cert_verify=False,
-                              debug=True, dsm_version=7,
-                              otp_code=parser.parse_args().otp)
-  print(fs.get_info())
+  args = parser.parse_args()
+  # fs = filestation.FileStation(SYNOLOGY_IP, SYNOLOGY_PORT, SYNOLOGY_USERNAME,
+  #                             SYNOLOGY_PASSWORD, secure=True, cert_verify=False,
+  #                             debug=True, dsm_version=7,
+  #                             otp_code=args.otp)
+
+  for exp_enum in Experiment.__iter__():
+    local_data_dir = get_dir_path(exp_enum.value, "data")
+    nas_data_dir_string = get_nas_exp_dir_string(exp_enum.value)
+
+    for exp_dir in local_data_dir.iterdir():
+      exp_dirname = exp_dir.stem
+      # fs.create_folder(folder_path=nas_data_dir_string, name=exp_dirname)
+  
+      for file in exp_dir.iterdir():
+        print(file)
+
+      # nas_exp_dir = get_nas_exp_dir(exp_enum.value)
+      # fs.upload_file(dest_path=f"home/Master's Thesis{nas_exp_dir}/Results",
+      #                file_path=data_dir.as_posix(),
+      #                overwrite=False,
+      #                progress_bar=True)

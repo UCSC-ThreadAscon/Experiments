4241675c4b0bf4e67d5795b5c3babb174bba2cf5
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-11-18 12:56:11 -0800

    Specify absolute path.

--- common/to_cipher_num.sh
+++
@@ -1,9 +0,0 @@
-# RESOURCES UTILIZED:
-# https://ryanstutorials.net/bash-scripting-tutorial/bash-if-statements.php#case
-#
-case $1 in
-  "AES") echo 0 ;;
-  "NoEncrypt") echo 1 ;;
-  "LibAscon-128a") echo 4 ;;
-  "LibAscon-128") echo 5;;
-esac

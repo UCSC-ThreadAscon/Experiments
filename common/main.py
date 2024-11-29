4996c6d1bb67bff32dcc5dc2e80f92a90a025502
simeon <4740112+SimeonAT@users.noreply.github.com>
2024-11-29 12:57:33 -0800

    Don't print to the console.

--- common/main.py
+++ common/main.py
@@ -103,7 +103,8 @@ def server_monitor(tx_power, cipher_num, exp_server_num, exp_client_num, experim
     server_script = get_server_script(exp_server_num)
     subprocess.run(["bash", server_script, "-t", tx_power,
                    "-e", cipher_num, "-p", SERVER_PORT,
-                   "-x", exp_server_num])
+                   "-x", exp_server_num],
+                   stdout=PIPE, stderr=STDOUT)
 
     exp_dir_path = get_dir_path(experiment_num, None).as_posix()
 

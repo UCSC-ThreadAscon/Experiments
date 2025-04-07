d0059ce2c5cb7486a2d4bdc88a930115842c8b17
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-04-07 12:01:20 -0700

    Good progress

--- common/energy/sed.sh
+++ common/energy/sed.sh
@@ -36,9 +36,9 @@ function to_cipher_string() {
 
 function to_role() {
   case $1 in
-    1) "front-door" ;;
-    2) "air-quality" ;;
-    3) "window" ;;
+    1) echo "front-door" ;;
+    2) echo "air-quality" ;;
+    3) echo "window" ;;
   esac
 }
 
@@ -69,47 +69,26 @@ sed_path=$HOME/Desktop/Repositories/energy-usage-sed-simple
 # ---- Set the KConfig variables ----
 sed_sdkconfig=$sed_path/sdkconfig
 
-sdkconfig_set CONFIG_EXPERIMENT $experiment_num $sed_sdkconfig
+sdkconfig_set CONFIG_SCENARIO $role_num $sed_sdkconfig
 
 # Change both the cipher suite and TX power settings in `sdkconfig`.
 sdkconfig_set CONFIG_THREAD_ASCON_CIPHER_SUITE $cipher_num $sed_sdkconfig
 sdkconfig_set CONFIG_TX_POWER $tx_power $sed_sdkconfig
-
-time_sync_not_set="# CONFIG_OPENTHREAD_TIME_SYNC is not set"
-time_sync_set="CONFIG_OPENTHREAD_TIME_SYNC=1"
-if [ $experiment_num == 3 ] || [ $experiment_num == 4 ]
-then
-  sed -i -e "s/$time_sync_not_set/$time_sync_set/g" $ftd_sdkconfig
-else
-  sed -i -e "s/$time_sync_set/$time_sync_not_set/g" $ftd_sdkconfig
-fi
-
-# Make sure USB/Serial JTAG monitoring is ENABLED on the FTD.
-usb_serial_monitor_flag=$(cat $ftd_sdkconfig | grep 'CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG=')
-if [[ "$usb_serial_monitor_flag" != "CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG=y" ]]
-then
-  echo "ERROR: USB Serial/JTAG monitoring is NOT ENABLED on the FTD." |& tee -a $output_file_path
-  echo "Please turn the USB Serial/JTAG monitoring flag ON." |& tee -a $output_file_path
-  echo "$(cat $ftd_sdkconfig | grep 'CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG=')" |& tee -a $output_file_path
-  exit 1
-fi
 # -----------------------------------
 
 # ---- Build, Flash, & Monitor ----
-cd $ftd_path
+cd $sed_path
 
 echo "--------- FTD KConfig Variables ---------" |& tee -a $output_file_path
-echo $(sdkconfig_get CONFIG_THREAD_ASCON_CIPHER_SUITE $ftd_sdkconfig) |& tee -a $output_file_path
-echo $(sdkconfig_get CONFIG_TX_POWER $ftd_sdkconfig) |& tee -a $output_file_path
-echo $(sdkconfig_get CONFIG_EXPERIMENT $ftd_sdkconfig) |& tee -a $output_file_path
-echo $(cat sdkconfig | grep CONFIG_OPENTHREAD_TIME_SYNC) |& tee -a $output_file_path
-echo $(sdkconfig_get CONFIG_OPENTHREAD_MAC_DEFAULT_MAX_FRAME_RETRIES_DIRECT $ftd_sdkconfig) |& tee -a $output_file_path
+echo $(sdkconfig_get CONFIG_THREAD_ASCON_CIPHER_SUITE $sed_sdkconfig) |& tee -a $output_file_path
+echo $(sdkconfig_get CONFIG_TX_POWER $sed_sdkconfig) |& tee -a $output_file_path
+echo $(sdkconfig_get CONFIG_SCENARIO $sed_sdkconfig) |& tee -a $output_file_path
 echo "-----------------------------------------" |& tee -a $output_file_path
 
 source $HOME/esp/esp-idf/export.sh &>> $output_file_path
 
 idf.py fullclean |& tee -a $output_file_path
-idf.py build flash --port $ftd_port |& tee -a $output_file_path
+idf.py build flash --port $sed_port |& tee -a $output_file_path
 
 cd -
 # ---------------------------------
